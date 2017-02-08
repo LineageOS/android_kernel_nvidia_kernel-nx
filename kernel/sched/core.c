39361f66983f13eacdc508931ca2012dc2a038b5
Ingo Molnar <mingo@kernel.org>
2017-02-08 08:45:17 +0100

    UPSTREAM: sched/headers: Prepare for new header dependencies before moving code to <linux/sched/loadavg.h>
    We are going to split <linux/sched/loadavg.h> out of <linux/sched.h>, which
    will have to be picked up from a couple of .c files.
    
    Create a trivial placeholder <linux/sched/topology.h> file that just
    maps to <linux/sched.h> to make this patch obviously correct and
    bisectable.
    
    Include the new header in the files that are going to need it.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    (cherry picked from commit 4f17722c7256af8e17c2c4f29f170247264bdf48)
    
    Bug: 111308141
    Test: modified lmkd to use PSI and tested using lmkd_unit_test
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Change-Id: I6da8f80df8e4d9b919a56b286a86d5a72b189684

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -77,6 +77,7 @@
 #include <linux/prefetch.h>
 #include <linux/tegra_profiler.h>
 #include <linux/cpufreq_times.h>
+#include <linux/sched/loadavg.h>
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
