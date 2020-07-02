77e77ee25d2e2f0a5585aaa3d7bf2b798ede8b98
Chanwoo Choi <cw00.choi@samsung.com>
2020-07-02 20:41:28 +0900

    PM / devfreq: Add support delayed timer for polling mode
    Until now, the devfreq driver using polling mode like simple_ondemand
    governor have used only deferrable timer for reducing the redundant
    power consumption. It reduces the CPU wake-up from idle due to polling mode
    which check the status of Non-CPU device.
    
    But, it has a problem for Non-CPU device like DMC device with DMA operation.
    Some Non-CPU device need to do monitor continuously regardless of CPU state
    in order to decide the proper next status of Non-CPU device.
    
    So, add support the delayed timer for polling mode to support
    the repetitive monitoring. The devfreq driver and user can select
    the kind of timer on either deferrable and delayed timer.
    
    For example, change the timer type of DMC device
    based on Exynos5422-based Odroid-XU3 as following:
    
    - If want to use deferrable timer as following:
    echo deferrable > /sys/class/devfreq/10c20000.memory-controller/timer
    
    - If want to use delayed timer as following:
    echo delayed > /sys/class/devfreq/10c20000.memory-controller/timer
    
    Reviewed-by: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Reviewed-by: Lukasz Luba <lukasz.luba@arm.com>
    Signed-off-by: Chanwoo Choi <cw00.choi@samsung.com>

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -27,6 +27,13 @@
 #define	DEVFREQ_PRECHANGE		(0)
 #define DEVFREQ_POSTCHANGE		(1)
 
+/* DEVFREQ work timers */
+enum devfreq_timer {
+	DEVFREQ_TIMER_DEFERRABLE = 0,
+	DEVFREQ_TIMER_DELAYED,
+	DEVFREQ_TIMER_NUM,
+};
+
 struct devfreq;
 
 /**
@@ -68,6 +75,7 @@ struct devfreq_dev_status {
  * @initial_freq:	The operating frequency when devfreq_add_device() is
  *			called.
  * @polling_ms:		The polling interval in ms. 0 disables polling.
+ * @timer:		Timer type is either deferrable or delayed timer.
  * @target:		The device should set its operating frequency at
  *			freq or lowest-upper-than-freq value. If freq is
  *			higher than any operable frequency, set maximum.
@@ -103,6 +111,7 @@ struct devfreq_dev_status {
 struct devfreq_dev_profile {
 	unsigned long initial_freq;
 	unsigned int polling_ms;
+	enum devfreq_timer timer;
 
 	int (*target)(struct device *dev, unsigned long *freq, u32 flags);
 	int (*get_dev_status)(struct device *dev,
