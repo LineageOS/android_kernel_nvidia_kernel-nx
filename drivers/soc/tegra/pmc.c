3ecacd3335591c95cc08047251897197801f10b4
CTCaer <ctcaer@gmail.com>
2022-12-22 15:47:43 +0200

    soc: tegra: r2p: rewrite driver and use new api
    - Add support for T210B01
    - Use new SMC commands
    - Clean up and simplify supported userspace commands
    - Properly use moduel parameters
    
    Supported `action`s:
    self: reboots to self.
    bootloader: reboots to bootloader menu.
    normal: reboots normally (auto boot will be used if enabled)
    ums: reboots into selected UMS disk
    via-payload: deprecated, use `self` isntead.
    
    The rest are internal management for notifier.
    
    `param1/2` are used to configure autoboot index or ums disk.
    
    `entry_id` can only be used on T210, so it should be avoided.

--- drivers/soc/tegra/pmc.c
+++ drivers/soc/tegra/pmc.c
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2010 Google, Inc
  * Copyright (c) 2012-2018, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2021-2022, CTCaer
  *
  * Author:
  *	Colin Cross <ccross@google.com>
@@ -993,11 +994,10 @@ enum tegra_system_reset_level tegra_pmc_get_system_reset_level(void)
 	return tegra_rst_lvl_sts;
 }
 
-extern void r2p_setup(const char *cmd);
-
 static void tegra_pmc_program_reboot_reason(const char *cmd)
 {
 	u32 value;
+	u32 panic_code = tegra_pmc_reg_readl(TEGRA_PMC_SCRATCH37);
 
 	value = tegra_pmc_reg_readl(TEGRA_PMC_SCRATCH0);
 	value &= ~PMC_SCRATCH0_MODE_MASK;
@@ -1013,7 +1013,7 @@ static void tegra_pmc_program_reboot_reason(const char *cmd)
 
 	/* Setup R2P for T124/T210/T210B01 */
 	if (pmc->soc->supports_r2p) {
-		r2p_setup(cmd);
+		tegra_pmc_r2p_setup(cmd, panic_code == KERNEL_PANIC_MAGIC);
 
 		/* Deprecated: Old TZ support. */
 		value |= PMC_SCRATCH0_MODE_PAYLOAD;
